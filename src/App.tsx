3dbfacaad86663a7430d0be50f2889c282a6c6e3
FabricioHartmann <fabricio.hartmann@hotmail.com>
2025-12-15 13:36:18 -0300

    feat: testing modal implementation using zustand store

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { appRoutes } from "./routes";
+import { ModalRoot } from "./components/Modal/ModalRoot";
 
 function App() {
   return (
     <BrowserRouter>
+      <ModalRoot />
       <Routes>
         {appRoutes.map((route, index) => (
           <Route key={index} path={route.path} element={route.element} />
--- src/pages/Home/Home.page.tsx
+++ src/pages/Home/Home.page.tsx
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
 import { Flex, ProfileCard, QuestCardList } from "../../components";
 import useIsMobile from "../../hooks/useIsMobile/useIsMobile";
 import "./Home.styles.css";
+import { useModalStore } from "../../store/modal/modalStore";
+import { WelcomeModal } from "../../components/Modal/variants/WelcomeModal";
 
 export function Home() {
   const isMobile = useIsMobile();
+  const { openModal, closeModal } = useModalStore();
+
+  useEffect(() => {
+    openModal(
+      <WelcomeModal />
+    );
+  }, [openModal, closeModal]);
 
   return (
     <Flex justify="center" align="center" direction="column" gap="16px">
