ea3ef04375b2a96e5fe7d07bc9de7996ef61c8a2
FabricioHartmann <fabricio.hartmann@hotmail.com>
2026-01-05 17:20:09 -0300

    feat: add rituals route, navbar item and page

---
+++ src/pages/Rituals/Rituals.page.tsx
@@ -0,0 +1,10 @@
+import "./Rituals.style.css";
+
+export function Rituals() {
+  return (
+    <div className="rituals-page">
+      <div className="rituals-page-header"></div>
+      <div className=""></div>
+    </div>
+  );
+}
