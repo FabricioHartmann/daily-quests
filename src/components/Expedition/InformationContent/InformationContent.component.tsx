0ff8fa8b7688609ba0627de076b4d8261344bb3a
FabricioHartmann <fabricio.hartmann@hotmail.com>
2026-01-07 02:17:21 -0300

    fix: overall adjustments and fixes

--- src/components/Expedition/InformationContent/InformationContent.component.tsx
+++ src/components/Expedition/InformationContent/InformationContent.component.tsx
@@ -5,13 +5,13 @@ export function InformationContent() {
   return (
      <div className="expedition-information-text">
         <Text size="sm">
-          As expedições são inspiradas na técnica de Pomodoro, para te ajudar a
-          focar nas suas tarefas, trabalho, estudo, etc.
+          As expedições irão te ajudar a
+          focar nas suas batalhas usando a técnica Pomodoro.
         </Text>
 
         <Text size="sm">
           Ao iniciar, você entra em um período de 25 minutos de foco total,
-          seguido por 5 minutos de descanso para recuperar suas forças.
+          seguido por 5 minutos de descanso.
         </Text>
         <Text size="sm">
           Complete expedições para ganhar experiência e aumentar sua
